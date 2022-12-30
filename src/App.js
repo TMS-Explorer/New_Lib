09dd31e536eeefc27b04781ca41a3381692be060
TMS <71687440+TMS-Explorer@users.noreply.github.com>
2022-12-30 21:47:36 +0530

    App Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Hamburger from './Components/Hamburger/Hamburger';
+import Header from './Components/Header/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import BookList from './Components/BookList/BookList';
+import BookDetails from './Components/BookDetails/BookDetails';
+import Searchbar from './Components/SearchBar/SearchBar'
+import BookModel from './Models/BookModel'
 
 function App() {
+  let BookLists = BookModel;
+  const [searchResults, setSearchResults] = useState(BookLists);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Container>
+          <Row className='App-header'>
+            <Container>
+              <Row>
+                <Col xs={1}>
+                  <Hamburger /></Col>
+                <Col xs={11} className="center">
+                  <Header />
+                </Col>
+              </Row>
+              <Row>
+                <Searchbar books={BookLists} setSearchResults={setSearchResults} />
+              </Row>
+            </Container>
+          </Row>
+
+          <Row>
+
+            <Routes>
+
+              <Route exact path="/" element={<BookList  BKList={searchResults}/>} />
+              <Route path="/BookDetails" element={<BookDetails Books={BookLists} />} />
+
+            </Routes>
+
+          </Row>
+
+        </Container>
+      </div>
+    </Router>
   );
 }
 
